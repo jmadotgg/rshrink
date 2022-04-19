acefc1d7061773d7b6a8e35b6981e65108a6a355
Julius Makel <julius-makel@online.de>
2022-04-19 23:26:03 +0200

    Add options for gaussian blur and quality Clean up

---
+++ TODO.md
@@ -0,0 +1,11 @@
+## Todos
+
+- [x] Essential
+  - [x] parse user input
+  - [x] resizing images
+  - [ ] traverse files recursively
+- [ ] nice to have
+  - [ ] add ffmpeg
+  - [ ] multithreading?
+  - [ ] better error handling
+  - [ ] add ui
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ struct Dimensions {
     height: usize,
 }
 impl Dimensions {
+    fn new(width: usize, height: usize) -> Dimensions {
+        Dimensions { width, height }
+    }
     fn parse_dimensions(dimensions: &str) -> Result<Dimensions, &str> {
         let d: Vec<&str> = dimensions.split("x").collect();
         if let [width, height] = d[..] {
@@ -25,11 +28,29 @@ impl Dimensions {
         Err("Invalid dimensions!")
     }
 }
+
+// Targeted terminal command;
+//magick \
+//        $fullFileName \
+//        -sampling-factor 4:2:0 \
+//        -strip \
+//        -quality 85 \
+//        -interlace Plane \
+//        -gaussian-blur 0.05 \
+//        -colorspace RGB \
+//        $newFilePath
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long)]
     dimensions: Option<String>,
+
+    #[clap(short, long)]
+    gaussian_blur: Option<bool>,
+
+    #[clap(short, long, default_value = "85")]
+    compression_quality: usize,
 
     #[clap(default_value = DEFAULT_REGEX)]
     files: String,
@@ -41,33 +62,19 @@ struct Args {
     out_dir: String,
 }
 
-// impl Args {
-// fn get_dimensions(&self) -> Option<Result<Dimensions, &str>> {
-// if let Some(d) = &self.dimensions {
-// let dimensions: Vec<&str> = d.split("x").collect();
-// if let [width, height] = dimensions[..] {
-// return Some(Ok(Dimensions {
-// width: width.parse::<usize>().expect("Invalid width!"),
-// height: height.parse::<usize>().expect("Invalid height!"),
-// }));
-// }
-// return Some(Err("Invalid dimensions!"));
-// }
-// None
-// }
-// }
-
 fn main() {
     let Args {
         in_dir,
         out_dir,
         files,
         dimensions,
+        gaussian_blur,
+        compression_quality,
     } = Args::parse();
 
     println!(
-        "In Directory: {}\nOut Directory: {}\nDimensions: {:?}\nFiles: {}",
-        in_dir, out_dir, &dimensions, files
+        " In Directory: {}\n Out Directory: {}\n Dimensions: {:?}\n File Regex: {}\n Compression quality: {}\n Gaussian blur: {:?}\n",
+        in_dir, out_dir, dimensions, files, compression_quality, gaussian_blur
     );
 
     match create_dir_if_not_exists(&out_dir) {
@@ -78,8 +85,6 @@ fn main() {
     // TODO: Print Error to console (More elegant way than using match?)
     let all_files = list_files(&in_dir).expect("Failed to read files!");
 
-    println!("All files {:?}", all_files);
-
     let selected_files = all_files
         .iter()
         .cloned()
@@ -89,16 +94,78 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    println!("Matched files {:?}", selected_files);
+    println!("Matched files {:?}\n", selected_files);
 
-    if let Some(d) = dimensions {
-        let d = Dimensions::parse_dimensions(&d).expect("Failed to parse dimensions!");
-        for file in selected_files {
-            if let Some(f) = file.to_str() {
-                resize(f, &out_dir, &d).expect(&format!("Failed to resize file! {f}"));
-            }
+    let dims = match &dimensions {
+        Some(d) => Dimensions::parse_dimensions(&d).expect("Failed to parse dimensions!"),
+        None => Dimensions::new(1920, 1080),
+    };
+
+    let apply_gaussian_blur = match gaussian_blur {
+        Some(v) => v,
+        None => false,
+    };
+
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
+
+    let mut wand = MagickWand::new();
+
+    println!("Progress:");
+
+    let file_count = selected_files.len();
+    for (i, file) in selected_files.iter().enumerate() {
+        println!(
+            "=> {}% [{:?}]",
+            ((i as f32 / file_count as f32) * 100.0).floor(),
+            file
+        );
+
+        // https://stackoverflow.com/questions/34837011/how-to-clear-the-terminal-screen-in-rust-after-a-new-line-is-printed
+        // print!("\x1B[2J\x1B[1;1H");
+        // print!("{esc}c", esc = 27 as char);
+
+        if let Some(file_name) = file.to_str() {
+            match shrink(
+                &mut wand,
+                file_name,
+                &out_dir,
+                &dims,
+                compression_quality,
+                apply_gaussian_blur,
+            ) {
+                Ok(()) => (),
+                Err(err) => eprintln!("Failed to shrink file {}! : {}", file_name, err),
+            };
         }
     }
+}
+
+fn shrink(
+    wand: &mut MagickWand,
+    file_name: &str,
+    out_dir: &str,
+    dims: &Dimensions,
+    compression_quality: usize,
+    apply_gaussian_blur: bool,
+) -> Result<(), MagickError> {
+    wand.read_image(&file_name)?;
+    wand.fit(dims.width, dims.height);
+
+    wand.set_sampling_factors(&[4.0, 2.0, 0.0])?;
+    wand.strip_image()?;
+    wand.set_image_compression_quality(compression_quality)?;
+    // 3 = Plane (build.rs)
+    wand.set_interlace_scheme(3)?;
+
+    if apply_gaussian_blur {
+        // Pretty slow
+        wand.gaussian_blur_image(0.05, 1.0)?
+    }
+
+    let new_file = format!("{}/sm_{}", out_dir, file_name);
+    wand.write_image(new_file.as_str())
 }
 
 fn create_dir_if_not_exists(dir: &str) -> io::Result<()> {
@@ -114,17 +181,6 @@ fn parse_file(file_sel: &str, file_name: &str) -> bool {
             false
         }
     }
-}
-fn resize(image_path: &str, out_dir: &str, dimensions: &Dimensions) -> Result<(), MagickError> {
-    START.call_once(|| {
-        magick_wand_genesis();
-    });
-
-    let wand = MagickWand::new();
-    wand.read_image(image_path)?;
-    wand.fit(dimensions.width, dimensions.height);
-    let new_file = format!("{}/sm_{}", out_dir, image_path);
-    wand.write_image(new_file.as_str())
 }
 
 fn list_files(path: &str) -> io::Result<Vec<OsString>> {
